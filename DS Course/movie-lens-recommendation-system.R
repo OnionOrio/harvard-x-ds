f22c043de4cd36bb34cceca5474ca47d8482ac81
OnionOrio <44561215+OnionOrio@users.noreply.github.com>
2020-11-12 16:26:00 +0800

    Add files via upload

--- DS Course/movie-lens-recommendation-system.R
+++ DS Course/movie-lens-recommendation-system.R
@@ -136,3 +136,167 @@ RMSE(predicted_ratings, test_set$rating)
 
 rmse_results <- rmse_results %>% add_row(method = "Added User effects", RMSE = RMSE(predicted_ratings, test_set$rating))
 rmse_results
+
+# Regularization to penalize the large estimate on movie effects with a small sample size -> penalize least square
+lambda <- 3
+mu <- mean(train_set$rating)
+movie_reg_avgs <- train_set %>% 
+  group_by(movieId) %>% 
+  summarize(b_i = sum(rating - mu)/(n()+lambda), n_i = n()) 
+
+tibble(original = movie_avgs$b_i, 
+       regularlized = movie_reg_avgs$b_i, 
+       n = movie_reg_avgs$n_i) %>%
+  ggplot(aes(original, regularlized, size=sqrt(n))) + 
+  geom_point(shape=1, alpha=0.5)
+
+movie_titles <- edx %>% select(movieId, title) %>% distinct()
+
+train_set %>%
+  count(movieId) %>% 
+  left_join(movie_reg_avgs, by = "movieId") %>%
+  left_join(movie_titles, by = "movieId") %>%
+  arrange(desc(b_i)) %>% 
+  slice(1:10) %>% 
+  pull(title)
+
+train_set %>%
+  count(movieId) %>% 
+  left_join(movie_reg_avgs, by = "movieId") %>%
+  left_join(movie_titles, by="movieId") %>%
+  arrange(b_i) %>% 
+  select(title, b_i, n) %>% 
+  slice(1:10) %>% 
+  pull(title)
+
+predicted_ratings <- test_set %>% 
+  left_join(movie_reg_avgs, by = "movieId") %>%
+  left_join(user_avgs, by='userId') %>%
+  mutate(pred = mu + b_i) %>%
+  pull(pred)
+RMSE(predicted_ratings, test_set$rating)
+rmse_results <- rmse_results %>% add_row(method = "Regularized Movie effects",RMSE= RMSE(predicted_ratings, test_set$rating))
+rmse_results
+
+# Cross validation on lamda - the penalize terms
+lambdas <- seq(0, 10, 0.25)
+
+mu <- mean(train_set$rating)
+just_the_sum <- train_set %>% 
+  group_by(movieId) %>% 
+  summarize(s = sum(rating - mu), n_i = n())
+#> `summarise()` ungrouping output (override with `.groups` argument)
+
+rmses <- sapply(lambdas, function(l){
+  predicted_ratings <- test_set %>% 
+    left_join(just_the_sum, by='movieId') %>% 
+    mutate(b_i = s/(n_i+l)) %>%
+    mutate(pred = mu + b_i) %>%
+    pull(pred)
+  return(RMSE(predicted_ratings, test_set$rating))
+})
+qplot(lambdas, rmses)  
+lambdas[which.min(rmses)]
+
+# Redo the regularization with the best fit lambda
+
+lamda <- 1.5
+movie_reg_avgs <- train_set %>% 
+  group_by(movieId) %>% 
+  summarize(b_i = sum(rating - mu)/(n()+lambda), n_i = n()) 
+
+user_avgs_after_reg_avg <- train_set %>% 
+  left_join(movie_reg_avgs, by='movieId') %>%
+  group_by(userId) %>%
+  summarize(b_u = mean(rating - mu_hat - b_i))
+
+tibble(original = movie_avgs$b_i, 
+       regularlized = movie_reg_avgs$b_i, 
+       n = movie_reg_avgs$n_i) %>%
+  ggplot(aes(original, regularlized, size=sqrt(n))) + 
+  geom_point(shape=1, alpha=0.5)
+
+predicted_ratings <- test_set %>% 
+  left_join(movie_reg_avgs, by = "movieId") %>%
+  left_join(user_avgs_after_reg_avg, by='userId') %>%
+  mutate(pred = mu + b_i + b_u) %>%
+  pull(pred)
+RMSE(predicted_ratings, test_set$rating)
+rmse_results <- rmse_results %>% add_row(method = "Regularized Movie effects with cross validation with User effects updated",RMSE= RMSE(predicted_ratings, test_set$rating))
+rmse_results
+
+
+# Matrix Factorization for group of movie effects and group of user pattern
+train_small <- train_set %>% 
+  group_by(movieId) %>%
+  filter(n() >= 150 | movieId == 3252) %>% ungroup() %>% 
+  group_by(userId) %>%
+  filter(n() >= 150) %>% ungroup()
+
+y <- train_small %>% 
+  select(userId, movieId, rating) %>%
+  spread(movieId, rating) %>%
+  as.matrix()
+
+rownames(y)<- y[,1]
+y <- y[,-1]
+
+colnames(y) <- with(movie_titles, title[match(colnames(y), movieId)])
+
+# Removing colmean and rowmean -> convert them into residuals
+y <- sweep(y, 2, colMeans(y, na.rm=TRUE))
+y <- sweep(y, 1, rowMeans(y, na.rm=TRUE))
+
+m_1 <- "Toy Story (1995)"
+m_2 <- "Jumanji (1995)"
+p1 <- qplot(y[ ,m_1], y[,m_2], xlab = m_1, ylab = m_2)
+
+m_1 <- "Toy Story (1995)"
+m_3 <- "Grumpier Old Men (1995)"
+p2 <- qplot(y[ ,m_1], y[,m_3], xlab = m_1, ylab = m_3)
+
+m_4 <- "Toy Story (1995)" 
+m_5 <- "Magnificent Seven, The (1960)" 
+p3 <- qplot(y[ ,m_4], y[,m_5], xlab = m_4, ylab = m_5)
+
+gridExtra::grid.arrange(p1, p2 ,p3, ncol = 3)
+
+rmse_results
+
+# Time effect
+
+library(lubridate)
+
+train_set <- train_set %>% mutate(date = as_datetime(timestamp))
+
+train_set %>% mutate(date = round_date(date, unit = "week")) %>%
+  group_by(date) %>%
+  summarize(rating = mean(rating)) %>%
+  ggplot(aes(date, rating)) +
+  geom_point() +
+  geom_smooth()
+
+test_set <- test_set %>% mutate(date = as_datetime(timestamp))
+
+time_effect_wk <- train_set %>% mutate(date = round_date(date, unit = "week")) %>% 
+  left_join(movie_reg_avgs, by = "movieId") %>%
+  left_join(user_avgs_after_reg_avg, by='userId') %>%
+  group_by(date) %>% 
+  summarize(f_t = mean(rating - mu_hat - b_i - b_u))
+  
+time_effect_wk
+
+train_set %>%  pull(date)
+
+predicted_ratings <- test_set %>% mutate(date = round_date(date, unit ="week")) %>% 
+  left_join(movie_reg_avgs, by = "movieId") %>%
+  left_join(user_avgs_after_reg_avg, by='userId') %>%
+  left_join(time_effect_wk, by='date') %>%
+  mutate(pred = mu + b_i + b_u + f_t) %>%
+  pull(pred)
+RMSE(predicted_ratings, test_set$rating)
+rmse_results <- rmse_results %>% add_row(method = "Added Weekly Time effects",RMSE= RMSE(predicted_ratings, test_set$rating))
+rmse_results
+
+
+# Genre Effect
